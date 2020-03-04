a7dda71d1165095468a0f1da0658024109e645bd
1064517852@qq.com <w1994z0115x>
2020-03-04 14:54:32 +0800

    引入seata但是未整合

---
+++ draven-spring-cloud-alibaba/draven-spring-cloud-alibaba-hoxton/noxus-draven-alibaba-hoxton-provider/src/main/java/com/noxus/draven/alibaba/hoxton/provider/controller/ProviderController.java
@@ -0,0 +1,21 @@
+package com.noxus.draven.alibaba.hoxton.provider.controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RefreshScope
+@RequestMapping("/provider")
+public class ProviderController {
+
+    @GetMapping(value = "/getString")
+    public String getString() {
+
+        System.out.println("111111");
+
+        return "11";
+    }
+}
