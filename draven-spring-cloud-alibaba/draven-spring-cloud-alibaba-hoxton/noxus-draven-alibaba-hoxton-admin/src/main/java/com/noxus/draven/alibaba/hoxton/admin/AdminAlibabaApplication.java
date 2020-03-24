430126a84f80227d6a311d228aefe29b3970f42f
1064517852@qq.com <w1994z0115x>
2020-03-24 19:20:10 +0800

    admin 监控

---
+++ draven-spring-cloud-alibaba/draven-spring-cloud-alibaba-hoxton/noxus-draven-alibaba-hoxton-admin/src/main/java/com/noxus/draven/alibaba/hoxton/admin/AdminAlibabaApplication.java
@@ -0,0 +1,32 @@
+package com.noxus.draven.alibaba.hoxton.admin;
+
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+@EnableAdminServer
+@RefreshScope
+public class AdminAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminAlibabaApplication.class, args);
+    }
+
+
+    @GetMapping("/testAdmin")
+    @SentinelResource("testAdmin")
+    public Mono<String> testConfig() {
+        return Mono.just("testAdmin");
+    }
+}
