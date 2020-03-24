4368c86cc8642c1c2a374f2316e4c44f5ef4877c
1064517852@qq.com <w1994z0115x>
2020-03-24 19:20:45 +0800

    admin监控

--- draven-spring-cloud-alibaba/draven-spring-cloud-alibaba-hoxton/noxus-draven-alibaba-hoxton-admin/src/main/java/com/noxus/draven/alibaba/hoxton/admin/AdminAlibabaApplication.java
+++ draven-spring-cloud-alibaba/draven-spring-cloud-alibaba-hoxton/noxus-draven-alibaba-hoxton-admin/src/main/java/com/noxus/draven/alibaba/hoxton/admin/AdminAlibabaApplication.java
@@ -4,7 +4,6 @@ package com.noxus.draven.alibaba.hoxton.admin;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
